13cdf379bdb74abb4d2cd82c6eae65b171661a41
Abelladius <maila@DESKTOP-E0LVPT1.(none)>
2021-09-13 06:12:32 -0700

    feat: connect frontend to backend

--- src/components/create-recipe.component.js
+++ src/components/create-recipe.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
-
+import axios from 'axios';
 
 export default class CreateRecipe extends Component {
     constructor(props) {
@@ -23,10 +23,16 @@ export default class CreateRecipe extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        })
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                }
+            })
+
     }
 
     onChangeUsername(e) {
@@ -61,6 +67,9 @@ export default class CreateRecipe extends Component {
         }
 
         console.log(recipe)
+
+        axios.post('http://localhost:5000/recipes/add', recipe)
+            .then(res => console.log(res.data));
 
         window.location = '/'
     }
--- src/components/recipes-list.component.js
+++ src/components/recipes-list.component.js
@@ -1,10 +1,73 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+
+const Recipe = props => (
+    <tr>
+        <td>{props.recipe.username}</td>
+        <td>{props.recipe.description}</td>
+        <td>{props.recipe.duration}</td>
+        <td>{props.recipe.date.substring(0, 10)}</td>
+        <td>
+            <Link to={"/edit/" + props.recipe._id}>edit</Link> | <a href="#" onClick={() => { props.deleteRecipe(props.recipe._id) }}>delete</a>
+        </td>
+    </tr>
+)
+
 
 export default class RecipesList extends Component {
+    constructor(props) {
+        super(props);
+        this.deleteRecipe = this.deleteRecipe.bind(this)
+
+        this.state = { recipes: [] }
+    }
+
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/recipes/')
+            .then(response => {
+                this.setState({ recipes: response.data })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    deleteRecipe(id) {
+        axios.delete('http://localhost:500/recipes/' + id)
+            .then(res => console.log(res.data))
+
+        this.setState({
+            recipes: this.state.recipes.filter(el => el._id !== id)
+        })
+    }
+
+    recipeList() {
+        return this.state.recipes.map(currentrecipe => {
+            return <Recipe recipe={currentrecipe} deleteRecipe={this.deleteRecipe} key={currentrecipe._id} />
+        })
+    }
+
     render() {
         return (
             <div>
-                <p>Pagina cu lista retetelor:</p>
+                <h3>Logged Exercises</h3>
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Description</th>
+                            <th>Duration</th>
+                            <th>Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.recipeList()}
+                    </tbody>
+                </table>
             </div>
         )
     }
