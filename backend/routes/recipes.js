d9892356360e18d180f78cebee1162dd62908a79
Abelladius <maila@DESKTOP-E0LVPT1.(none)>
2021-09-13 02:06:56 -0700

    feat: add backend

---
+++ backend/routes/recipes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router()
+let Recipe = require('../models/recipe.model');
+
+router.route('/').get((req, res) => {
+    Recipe.find()
+        .then(recipes => res.json(recipes))
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date)
+
+    const newRecipe = new Recipe({
+        username,
+        description,
+        duration,
+        date,
+    })
+
+    newRecipe.save()
+        .then(() => res.json('Recipe added!'))
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/:id').get((req, res) => {
+    Recipe.findById(req.params.id)
+        .then(recipe => res.json(recipe))
+        .catch(err => err.status(400).json('Error: ' + err))
+})
+
+router.route('/:id').delete((req, res) => {
+    Recipe.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Recipe deleted'))
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/update/:id').post((req, res) => {
+    Recipe.findById(req.params.id)
+        .then(recipe => {
+            recipe.username = req.body.username;
+            recipe.description = req.body.description;
+            recipe.duration = Number(req.body.duration);
+            recipe.date = Date.parse(req.body.date)
+
+            recipe.save()
+                .then(() => res.json('Exercise updated!'))
+                .catch(err => res.status(400).json('Error: ' + err))
+        })
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri,
+    err => {
+        if (err) throw err;
+        console.log('connected to MongoDB')
+    });
+
+const recipesRouter = require('./routes/recipes')
+const usersRouter = require('./routes/users')
+
+app.use('/recipes', recipesRouter)
+app.use('/users', usersRouter)
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port} `)
+})
